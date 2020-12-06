d16c2750e83e1be13b812eb299394bd6c5d1e81b
rubensmesquita <signerproducoes@gmail.com>
2020-12-06 14:59:28 -0300

    Added fonts, icons, and global css settings

--- web/src/Netflix.vue
+++ web/src/Netflix.vue
@@ -1,5 +1,17 @@
 <template>
   <div id="netflix">
+    <NavBar />
     <router-view/>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import NavBar from '@/components/navbar.vue'
+
+@Component({
+  components: { NavBar }
+})
+
+export default class Home extends Vue {}
+</script>
---
+++ web/src/components/navbar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="navbar">
+    <div class="nav-container">
+      <!-- Logo small -->
+      <a aria-label="Netflix" class="logo-sm icon-logo-sm" href="/"></a>
+      <ul class="menu-list l-navigation">
+        <li class="nav-menu">
+          <a class="menu-trigger" role="button" aria-haspopup="true" tabindex="0">Navegar</a>
+        </li>
+        <li class="nav-tab"><a class="active" href="/">Home</a></li>
+        <li class="nav-tab"><a href="/series">Series</a></li>
+        <li class="nav-tab"><a href="/movies">Movies</a></li>
+      </ul>
+      <div class="r-navigation">
+        <div class="nav-elem">
+          <div class="search-button">
+            <span class="icon-search"></span>
+          </div>
+        </div>
+        <!-- <div class="nav-elem">
+          <a href="/Kids">INFANTIL</a>
+        </div> -->
+        <div class="nav-elem">
+          <div class="notification-button">
+            <span class="icon-notification"></span>
+          </div>
+        </div>
+        <!-- Account button -->
+        <div class="nav-elem">
+          <div class="account-menu">
+            <div class="account-dropdown">
+              <a href="/" role="button" tabindex="0" aria-haspopup="true" aria-expanded="false" aria-label="Marcos - My account">
+                <span class="profile-link" role="presentation"><img class="profile-icon" :src="require('../assets/images/profile-img.png')" alt=""></span>
+              </a>
+              <span class="caret" role="presentation"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class NavBar extends Vue {
+}
+</script>
